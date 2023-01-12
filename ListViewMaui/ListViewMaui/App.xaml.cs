b014d8abb3f5aba86955611e45b59b636dc18db7
Suja Venkatesan <suja.venkatesan@syncfusion.com>
2023-01-12 11:28:08 +0530

    MAUI-4256 : How to bind data using FreshMVVM in .Net Maui ListView (SfListView)

---
+++ ListViewMaui/ListViewMaui/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using FreshMvvm.Maui;
+
+namespace ListViewMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+        var page = FreshPageModelResolver.ResolvePageModel<ListViewPageModel>();
+        var basicNavContainer = new FreshNavigationContainer(page);
+        MainPage = basicNavContainer;
+
+    }
+}
